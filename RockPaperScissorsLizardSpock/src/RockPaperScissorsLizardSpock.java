a937e3f50273d9a6c00b8b5b249f306d3a7a5232
Pavlin Malyushki <malyushki@gmail.com>
2022-09-23 20:38:39 +0300

    ** added point system ** added cheat codes ** added questions

--- RockPaperScissorsLizardSpock/src/RockPaperScissorsLizardSpock.java
+++ RockPaperScissorsLizardSpock/src/RockPaperScissorsLizardSpock.java
@@ -12,25 +12,78 @@ public class RockPaperScissorsLizardSpock {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("WELCOME TO ROCK, PAPER, SCISSORS, LIZARD, SPOCK");
-        System.out.println("Do you want to set up how many games you need to win?");
+        System.out.println("What is your name?");
+        String playerName = scanner.nextLine();
+        System.out.printf("Hello %s, do you want to set up how many points you need to win the whole game?%n",playerName);
         System.out.print("[Y]es or press a key to continue:");
         String setNumberOfWins = scanner.nextLine();
+        int banUseOf = 10;
+        while (setNumberOfWins.equals("cOdEs")) {
+            System.out.println("********************SECRET*****CODES**************************");
+            System.out.println("**************************************************************");
+            System.out.println("***BAN COMPUTER TO USE ROCK -> write *THEROCKISNOTCOKING* ****");
+            System.out.println("***BAN COMPUTER TO USE PAPER -> write *ENDLESSWATERFALL* *****");
+            System.out.println("***BAN COMPUTER TO USE SCISSORS -> write *REMOVESCREW* *******");
+            System.out.println("***BAN COMPUTER TO USE LIZARD -> write *LIZARDEATSLIZARD* ****");
+            System.out.println("***BAN COMPUTER TO USE SPOCK -> write *'LONGANDPROSPER* ******");
+            System.out.println("**************************************************************");
+            System.out.println("**************************************************************");
+            System.out.print("ENTER CODE: ");
+            setNumberOfWins = scanner.nextLine();
+            switch (setNumberOfWins){
+                case "THEROCKISNOTCOKING":
+                    banUseOf = 0;
+                    setNumberOfWins = "y";
+                    System.out.println("THE ROCK IS **GONE**");
+                    break;
+                case "ENDLESSWATERFALL":
+                    banUseOf = 1;
+                    setNumberOfWins = "y";
+                    System.out.println("THE PAPER IS **DAMAGED**");
+                    break;
+                case "REMOVESCREW":
+                    banUseOf = 2;
+                    setNumberOfWins = "y";
+                    System.out.println("THE SCISSORS IS **BROKEN**");
+                    break;
+                case "LIZARDEATSLIZARD":
+                    banUseOf = 3;
+                    setNumberOfWins = "y";
+                    System.out.println("THE LIZARD IS **EATEN**");
+                    break;
+                case "LONGANDPROSPER":
+                    banUseOf = 4;
+                    setNumberOfWins = "y";
+                    System.out.println("THE SPOCK IS **DEAD**");
+                    break;
+            }
+
+        }
         int n = 5;    // number of games player needs to win the whole game
         if (setNumberOfWins.toLowerCase(Locale.ROOT).equals("y")){
+            System.out.print("How many points: ");
             while (!scanner.hasNextInt()) {
                 System.out.println("Input is not a number.");
                 scanner.nextLine();
             }
+
             n = scanner.nextInt();
+            while (n<=0){
+                System.out.println("You need to enter a number bigger than 0!");
+                System.out.print("Try again: ");
+                n = scanner.nextInt();
+            }
+            System.out.printf("You must win %d points to win the whole game.%n",n);
             scanner.nextLine();
         }else {
-            System.out.println("You must win 5 times more than the computer to win the whole game");
+            System.out.println("You must win 5 points to win the whole game.");
         }
 
         System.out.println("Choose [r]ock, [p]aper, [s]cissors, [l]izard, [sp]ock: ");
         String playerMove = scanner.nextLine();
         int playerPoints = 0;
         while (!playerMove.toLowerCase(Locale.ROOT).equals("end")) {
+            boolean emptySlot = false;
             switch (playerMove.toLowerCase(Locale.ROOT)) {
                 case "r":
                 case "rock":
@@ -54,10 +107,18 @@ public class RockPaperScissorsLizardSpock {
                     break;
                 default:
                     System.out.println("Invalid input. Try again...");
+                    emptySlot = true;
                     break;
             }
+
             Random random = new Random();
             int computerRandomNumber = random.nextInt(5);
+            while (computerRandomNumber==banUseOf) {
+                computerRandomNumber = random.nextInt(5);
+                System.out.println("Computer is thinking...");
+                System.out.println("Computer forgot what to use...");
+                System.out.println("Computer is going to choose something else.");
+            }
             String computerMove = "";
             switch (computerRandomNumber) {
                 case 0:
@@ -75,6 +136,11 @@ public class RockPaperScissorsLizardSpock {
                 case 4:
                     computerMove = SPOCK;
                     break;
+            }
+            if (!emptySlot) {
+                System.out.println(playerName + " chooses: " + playerMove);
+                System.out.println("Computer chooses: " + computerMove);
+
             }
             if (playerMove.equals(computerMove)) {
                 System.out.println("It's a draw. Try again...");
